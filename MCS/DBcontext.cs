99ff0f377299d00a6560d9b9573ed9faa744d01f
Daylan Smith <daylan9046@gmail.com>
2018-06-15 10:50:08 -0500

    initial commit

---
+++ MCS/DBcontext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+
+namespace RetentionDraft2
+{
+    [Table(Name = "dbo.Department")]
+    public class Department
+    {
+        [Column(IsPrimaryKey = true)]
+        public string Name { get; set; }
+        [Column(CanBeNull = false)]
+        public int Number { get; set; }
+    }
+
+    [Table(Name = "dbo.Classification")]
+    public class Classification
+    {
+        [Column(IsPrimaryKey = true)]
+        public string DepartmentName { get; set; }
+        [Column(IsPrimaryKey = true)]
+        public string ClassificationName { get; set; }
+        [Column(CanBeNull = false)]
+        public int Number { get; set; }
+        [Column(CanBeNull = true)]
+        public double? YearsRetained { get; set; }
+    }
+
+    [Table(Name = "dbo.Title")]
+    public class Title
+    {
+        [Column(IsPrimaryKey = true)]
+        public string DepartmentName { get; set; }
+        [Column(IsPrimaryKey = true)]
+        public string ClassificationName { get; set; }
+        [Column(IsPrimaryKey = true, Name = "Title")]
+        public string MyTitle { get; set; }
+    }
+
+    [Table(Name = "dbo.Box")]
+    public class Box
+    {
+        [Column(IsPrimaryKey = true)]
+        public int BoxNumber { get; set; }
+        [Column(CanBeNull = false)]
+        public string DepartmentName { get; set; }
+        [Column(CanBeNull = false)]
+        public string ClassificationName { get; set; }
+        [Column(CanBeNull = false)]
+        public string StorageMedium { get; set; }
+        [Column(CanBeNull = true)]
+        public string RackNumber { get; set; }
+        [Column(CanBeNull = true)]
+        public string DepreciatedNumber { get; set; }
+        [Column(CanBeNull = false)]
+        public string Description { get; set; }
+    }
+
+    [Table(Name = "dbo.Record")]
+    public class Record
+    {
+        [Column(IsPrimaryKey = true)]
+        public int BoxNumber { get; set; }
+        [Column(IsPrimaryKey = true)]
+        public string RecordID { get; set; }
+        [Column(CanBeNull = false)]
+        public string Title { get; set; }
+        [Column(CanBeNull = false)]
+        public DateTime StartDate { get; set; }
+        [Column(CanBeNull = false)]
+        public DateTime EndDate { get; set; }
+        [Column(CanBeNull = false)]
+        public bool IsExecutive { get; set; }
+        [Column(CanBeNull = true)]
+        public DateTime? RetainUntil { get; set; }
+        [Column(CanBeNull = false)]
+        public string Notes { get; set; }
+        [Column(CanBeNull = false)]
+        public bool IsDestoryed { get; set; }
+        [Column(CanBeNull = true)]
+        public DateTime? DestoryDate { get; set; }
+        [Column(CanBeNull = true)]
+        public string DestoryAuthorization { get; set; }
+        [Column(CanBeNull = false)]
+        public bool IsHeld { get; set; }
+        [Column(CanBeNull = true)]
+        public DateTime? HoldDate { get; set; }
+        [Column(CanBeNull = true)]
+        public string HoldNotes { get; set; }
+    }
+}
---
+++ MCS/EPPlusLink.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.IO;
+using OfficeOpenXml;
+using System;
+
+namespace RetentionDraft2
+{
+    public static class EPPlusLink
+    {
+        public static string ExportToExcel(IQueryable<Record> records, string title, string path = "Temp.xlsx") {
+            ExcelPackage output = new ExcelPackage();
+            var worksheet = output.Workbook.Worksheets.Add(title);
+
+            string[] titlearray = { "Box Number", "Record ID", "Record Title", "Start Date","End Date", "Document Type",
+                "Destory Date", "Notes", "Destoryed?", "Date Destoryed", "Authorized By", "Held?", "Date Held", "Hold Notes" };
+
+            var formatrecord = from r in records
+                               select new {
+                                   r.BoxNumber,
+                                   r.RecordID,
+                                   r.Title,
+                                   Start = r.StartDate.ToString(),
+                                   End = r.EndDate.ToString(),
+                                   DocType = (r.IsExecutive ? "Executive" : "General"),
+                                   Retain = r.RetainUntil.ToString(),
+                                   r.Notes,
+                                   Destoryed = (r.IsDestoryed ? "Yes" : "No"),
+                                   Destory = r.DestoryDate.ToString(),
+                                   r.DestoryAuthorization,
+                                   Held = (r.IsHeld ? "Yes" : "No"),
+                                   Hold = r.HoldDate.ToString(),
+                                   r.HoldNotes
+                               };
+
+            worksheet.Cells["A1"].Value = title;
+            worksheet.Cells["A2"].Value = "Generated on";
+            worksheet.Cells["B2"].Value = DateTime.Now.ToString();
+            worksheet.Cells["A4"].LoadFromCollection(titlearray);
+            worksheet.Cells["A5"].LoadFromCollection(formatrecord);
+            worksheet.Cells.AutoFitColumns();
+
+            output.SaveAs(new FileInfo(path));
+            return path;
+        }
+    }
+}
